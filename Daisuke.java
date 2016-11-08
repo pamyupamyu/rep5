239ee8a3c731b856abc7e4253e5da34bc7a09530
Daisuke <d.s.key0130@gmail.com>
2016-11-08 09:02:48 +0900

    真面目な話
    “friend”と言う関係で友人関係を結んでみたけどこんな感じですかね？

--- Daisuke.java
+++ Daisuke.java
@@ -7,40 +7,61 @@ public class Daisuke {
     public static void main(String args[]){
 	SemanticNet sn = new SemanticNet();
 
-	// 野球はスポーツである．
-	sn.addLink(new Link("is-a","baseball","sports",sn));
+	// 大輔は男性である．
+	sn.addLink(new Link("is-a","Daisuke","man",sn));
+	
+	// 男性は人間である．
+		sn.addLink(new Link("is-a","man","human",sn));
 
-	// 太郎は名古屋工業大学の学生である．
-	sn.addLink(new Link("is-a","Taro","NIT-student",sn));
+	// 大輔は名古屋工業大学の学生である．
+	sn.addLink(new Link("is-a","Daisuke","NIT-student",sn));
 
+<<<<<<< Updated upstream
 	// 太郎の専門は人工知能である．
 	sn.addLink(new Link("speciality","Simano","AI",sn));
+=======
+	// 大輔の専門は人工知能である．
+	sn.addLink(new Link("speciality","Daisuke","AI",sn));
+>>>>>>> Stashed changes
 	
-	// フェラーリは車である．
-	sn.addLink(new Link("is-a","Ferrari","car",sn));
+	// エロゲはゲームである．
+	sn.addLink(new Link("is-a","Hentai-Game","Game",sn));
 
-	// 車はエンジンを持つ．
-	sn.addLink(new Link("has-a","car","engine",sn));
+	// 人間は知性を持つ．
+	sn.addLink(new Link("has-a","human","intelligence",sn));
 	
-	// 太郎の趣味は野球である．
-	sn.addLink(new Link("hobby","Taro","baseball",sn));
+	// 大輔の趣味はエロゲである．
+	sn.addLink(new Link("hobby","Daisuke","Hentai-Game",sn));
 	
-	// 太郎はフェラーリを所有する．
-	sn.addLink(new Link("own","Taro","Ferrari",sn));
+	// 大輔はエロゲを所有する．
+	sn.addLink(new Link("own","Daisuke","Hentai-Game",sn));
 
 	// 名古屋工業大学の学生は，学生である．
 	sn.addLink(new Link("is-a","NIT-student","student",sn));
 
 	// 学生は勉強しない．
 	sn.addLink(new Link("donot","student","study",sn));
+	
+	//大輔は広大の友達である。
+	sn.addLink(new Link("friend","Daisuke","Koudai",sn));
+	
+	//大輔は涼太の友達である。
+	sn.addLink(new Link("friend","Daisuke","Ryouta",sn));
+	
+	//大輔は知道の友達である。
+	sn.addLink(new Link("friend","Daisuke","Tomomiti",sn));
+	
+	//大輔は健太の友達である。
+	sn.addLink(new Link("friend","Daisuke","Kenta",sn));
+		
 
 	sn.printLinks();
 	sn.printNodes();
 
 	ArrayList<Link> query = new ArrayList<Link>();
-	query.add(new Link("own","?y","Ferrari"));
+	query.add(new Link("own","?y","Hentai-Game"));
 	query.add(new Link("is-a","?y","student"));
-	query.add(new Link("hobby","?y","baseball"));
+	query.add(new Link("hobby","?y","Hentai-Game"));
 	sn.query(query);
 	    System.out.print("aaa");
     }    
